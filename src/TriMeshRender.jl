5e426959318e78690f99be0b6be3d24b31eb86a0
Lawless-M <matt-github@steponnopets.net>
2022-01-30 19:56:10 +0000

    iso projection works, just needs orientation

--- src/TriMeshRender.jl
+++ src/TriMeshRender.jl
@@ -3,26 +3,28 @@ module TriMeshRender
 using TriMeshes
 using Luxor
 
-export isometric
+export isometric, render,  box!
+
+quad! = TriMeshes.quad!
 
 const r6 = sqrt(6)
 const rr6 = 1/r6
 const r2 = sqrt(2)
 const r3 = sqrt(3)
 
-const rM = [ [r3, 0, -r3] [1, 2, 1] [r2, r2, r2]]
+const rM = [ [r3, 1, r2] [0,2,r2] [-r3,1,r2] ]
 const Orth = [ [1,0,0] [0,1,0] [0,0,0] ]
 
 function isometric(v::Vertex) 
-	iv = Orth * rr6 .* rM * v
+	iv = Orth * rr6 * rM * v
 	Point(iv[1], iv[2])
 end
 
+isometric(n::Net) = map(isometric, n.vertices)
 
 function render(n::Net, w, h, fname)
 	@png begin
-	origin(0,0)
-	points = map(isometric, n.vertices)
+	points = isometric(n)
 	for f in n.faces
 		line(points[f.AB.from], points[f.AB.to], :stroke)
 		line(points[f.BC.from], points[f.BC.to], :stroke)
